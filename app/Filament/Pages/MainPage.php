50ecc536545f91b4fc03f6aa8f9ef59031730841
priana13 <herian.sap@gmail.com>
2025-03-21 11:29:19 +0700

    tampilan table tugas hari ini

--- app/Filament/Pages/MainPage.php
+++ app/Filament/Pages/MainPage.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Kategori;
 use App\Models\LaporanHarian;
 use Filament\Pages\Page;
 
@@ -11,19 +12,37 @@ class MainPage extends Page
 
     protected static string $view = 'filament.pages.main-page';
 
-    protected static ?string $title = 'Laporan Hari Ini';
+    protected static ?string $title = 'Tugas Hari Ini';
+
+    public $tanggal;
+
+    public function mount(){
+
+        $this->tanggal = date('d M Y');
+    }
 
     /**
      * @return array<string, mixed>
      */
     protected function getViewData(): array
     {
-        $data_laporan = LaporanHarian::get();     
+        $data_laporan = LaporanHarian::take(3)->get();     
 
         return [
             'data_laporan' => $data_laporan,
+            'list_kategori' => Kategori::all()
         ];
         
+    }
+
+    public function prevDate(){
+
+        $this->tanggal = date('d M Y', strtotime($this->tanggal . ' -1 day'));
+    }
+
+    public function today(){
+
+        $this->tanggal = date('d M Y');
     }
 
 }
--- resources/views/filament/pages/main-page.blade.php
+++ resources/views/filament/pages/main-page.blade.php
@@ -13,17 +13,19 @@
     <div>
         <button
             type="button"
+            wire:click="prevDate"
             class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-200 focus:bg-primary-accent-200 focus:outline-none focus:ring-0 active:bg-primary-accent-200 motion-reduce:transition-none dark:bg-primary-300 dark:hover:bg-primary-400 dark:focus:bg-primary-400 dark:active:bg-primary-400">
             Sebelumnya
         </button>
         <button
             type="button"
+            wire:click="today"
             class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
             Hari Ini
         </button>
     </div>
 
-    <h2 class="text-xl">Tanggal : 19 Maret 2025</h2>
+    <h2 class="text-xl">Tanggal : <strong>{{ $tanggal }}</strong> </h2>
     
 </div>
 
@@ -34,14 +36,14 @@
       <div class="inline-block min-w-full lg:px-8">
         <div class="overflow-hidden">
           <table
-            class="min-w-full text-center text-sm font-light text-surface dark:text-white">
+            class="min-w-full text-sm font-light text-surface dark:text-white">
             <thead
               class="border-b border-neutral-200 bg-primary-100 font-medium dark:border-white/10">
               <tr>
-                <th scope="col" class=" px-6 py-4">Tugas</th>
+                <th scope="col" class=" px-6 py-4 text-left">Tugas</th>
                 <th scope="col" class=" px-6 py-4">Kategori</th>
                 <th scope="col" class=" px-6 py-4">Status</th>
-                <th scope="col" class=" px-6 py-4">Catatan</th>
+                <th scope="col" class=" px-6 py-4 text-left">Catatan</th>
               </tr>
             </thead>
             <tbody>
@@ -52,18 +54,55 @@
                 <td class="whitespace-nowrap  px-6 py-4 font-medium">
                     {{ $laporan->judul }}
                 </td>
-                <td class="whitespace-nowrap  px-6 py-4">
+                <td class="whitespace-nowrap  px-6 py-4 text-center">
                     {{ $laporan->kategori->title }}
                 </td>
-                <td class="whitespace-nowrap  px-6 py-4">
+                <td class="whitespace-nowrap  px-6 py-4 text-center">
                     {{ $laporan->status }}
                 </td>
                 <td class="whitespace-nowrap  px-6 py-4">
-                    {{ $laporan->deskripsi }}
+                    {!! $laporan->deskripsi !!}
                 </td>
               </tr>
 
             @endforeach
+
+
+            <tr class="border-b border-neutral-200 dark:border-white/10">
+              <td class="whitespace-nowrap  px-6 py-4 font-medium">
+                <input type="text" id="first_name" class="border-0 text-gray-900 text-sm rounded-md block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ketik Tugas" required autocomplete="off" />
+              </td>
+              <td class="whitespace-nowrap  px-6 py-4 text-center">
+                <select id="countries" class="border-0 border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option selected>Kategori</option>
+
+                  @foreach($list_kategori as $kategori)
+
+                    <option value="{{ $kategori->id }}">{{ $kategori->title }}</option>
+
+                  @endforeach
+                 
+
+                </select>
+              </td>
+              <td class="whitespace-nowrap  px-6 py-4 text-center">
+                <select id="countries" class="border-0 border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option selected>Status</option>
+                  <option value="US">Selesai</option>
+                  <option value="CA">Progress</option>   
+                  <option value="CA">Next</option>                
+                </select>
+              </td>
+              <td class="whitespace-nowrap  px-6 py-4">
+                {{-- <input type="text" id="first_name" class="border-0 border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Catatan" required autocomplete="off" /> --}}
+
+                <textarea rows="1" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border-0 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Catatan..."></textarea>
+
+
+              </td>
+            </tr>
+
+
 
             </tbody>
           </table>
